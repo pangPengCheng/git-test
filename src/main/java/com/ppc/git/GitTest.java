f84dc9d9d73935bae0c32878f1aae65242085d68
庞鹏程 <2089841594@qq.com>
2022-10-15 10:03:49 +0800

    second commit

--- src/main/java/com/ppc/git/GitTest.java
+++ src/main/java/com/ppc/git/GitTest.java
@@ -3,5 +3,6 @@ package com.ppc.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git2!");
     }
 }
